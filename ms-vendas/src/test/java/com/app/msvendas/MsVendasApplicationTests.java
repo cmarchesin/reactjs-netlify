f778b6070f79a916b64236fd5854a5f78a24cf09
Carlos Marchesin <marchesin.cam@gmail.com>
2021-10-12 10:30:37 -0300

    Project base created

---
+++ ms-vendas/src/test/java/com/app/msvendas/MsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.msvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
