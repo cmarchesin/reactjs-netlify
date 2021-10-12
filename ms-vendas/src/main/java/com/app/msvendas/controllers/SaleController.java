ed746a4becf6a22f7cf146f5e672f782318e0ba4
Carlos Marchesin <marchesin.cam@gmail.com>
2021-10-12 20:45:59 -0300

    API Rest Pageable

---
+++ ms-vendas/src/main/java/com/app/msvendas/controllers/SaleController.java
@@ -0,0 +1,26 @@
+package com.app.msvendas.controllers;
+
+import com.app.msvendas.dto.SaleDTO;
+import com.app.msvendas.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+        Page<SaleDTO> listSellers = service.findAll(pageable);
+        return ResponseEntity.ok(listSellers);
+    }
+
+}
