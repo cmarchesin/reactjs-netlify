fe28aa9e5b0baa58e4faa0b4012b0f332248b1f6
Carlos Marchesin <marchesin.cam@gmail.com>
2021-10-12 13:35:02 -0300

    Init Dashboard Vendas

---
+++ mf-vendas/src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">
+                    App desenvolvido por 
+                    <a href="https://github.com/cmarchesin" target="_blank" rel="noreferrer">Carlos Marchesin</a>
+                </p>
+                <p className="text-light">
+                    <small>
+                        <strong>App ReactJS</strong><br/>
+                        Vendas Online: 
+                        <a href="https://instagram.com/#" target="_blank" rel="noreferrer">@cmarchesin</a>
+                    </small>
+                </p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
